8d2cb05707c1333d5c75047a9d8317d2e8432180
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-01-22 13:10:18 -0500

    fixed model error. closes #3

--- src/main/java/trinsdar/powerchisels/ClientProxy.java
+++ src/main/java/trinsdar/powerchisels/ClientProxy.java
@@ -1,21 +1,14 @@
 package trinsdar.powerchisels;
 
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy{
-
-    @Override
-    public void preInit(FMLPreInitializationEvent event){
-        super.preInit(event);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
 
     @SideOnly(Side.CLIENT)
     @SubscribeEvent
